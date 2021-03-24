9ea9d13ad71e12c303434bbadbc183b8ab67d540
季悠然 <youranreus@qq.com>
2021-03-24 19:37:08 +0800

    修改操作逻辑

--- App/Note/Note.php
+++ App/Note/Note.php
@@ -59,10 +59,10 @@ class Note
         $this->database->insert("note",[
             "sid"=>$sid,
             "content"=>"Begin your story.",
-            "key"=> $key
+            "key"=> ""
         ]);
 
-        return ["content"=>"Begin your story.","key"=>$key];
+        return ["content"=>"Begin your story.","key"=>""];
     }
 
     /**
@@ -99,17 +99,24 @@ class Note
      */
     public function modifyNote($sid)
     {
-        $this->checkKey($sid);
+
+        $haveKey = $this->haveKey($sid);
+        if($haveKey != false)
+        {
+            $this->checkKey($haveKey);
+        }
+        else
+        {
+            $this->database->update("note",["key"=>$_GET["key"]],["sid"=>$sid]);
+        }
+
 
         if(!isset($_POST["content"]))
         {
             exit(json_encode("参数缺失"));
         }
 
-        $result = $this->database->update("note",[
-            "content"=>$_POST["content"]],[
-            "sid"=>$sid
-        ]);
+        $result = $this->database->update("note",["content"=>$_POST["content"]],["sid"=>$sid]);
 
         exit(json_encode($result->rowCount()));
     }
@@ -117,18 +124,35 @@ class Note
     /**
      * User: youranreus
      * Date: 2021/3/23 17:00
-     * @param $sid
+     * @param $key
      */
-    private function checkKey($sid)
+    private function checkKey($key)
     {
         if(!isset($_GET["key"]))
         {
             exit(json_encode("密钥缺失"));
         }
-        if (!$this->database->has("note", ["AND" => ["OR" => ["sid" => $sid], "key" => $_GET["key"]]]))
+        if ($key != $_GET["key"])
         {
             exit(json_encode("密钥错误或对应笔记不存在"));
         }
     }
+
+    /**
+     * @param $sid
+     * @return bool
+     * User: youranreus
+     * Date: 2021/3/24 19:22
+     */
+    private function haveKey($sid): bool
+    {
+        $key = $this->database->select("note","key",['sid'=>$sid]);
+        if($key[0] != "")
+        {
+            return $key[0];
+        }
+        return false;
+    }
+
 
 }
