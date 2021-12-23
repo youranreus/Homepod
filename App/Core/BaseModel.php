ad291de7def7f843d0e083f00aeca32701a3941c
youranreus <youranreus@qq.com>
2021-12-23 14:37:24 +0800

    update: Note便签模型

---
+++ App/Core/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Core;
+use App\Conf\Conf;
+use Medoo\Medoo;
+
+class BaseModel
+{
+    protected $database;
+
+    public function __construct()
+    {
+        $this->database = new medoo([
+            'database_type' => 'mysql',
+            'database_name' => Conf::$dbname,
+            'server' => Conf::$servername,
+            'username' => Conf::$username,
+            'password' => Conf::$password,
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+        ]);
+    }
+}
